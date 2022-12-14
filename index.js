01c35c4d08b71e03eed46de867bd07403664920d
alexeiMikhail <leleuxc@gmail.com>
2022-12-14 15:39:12 -0600

    Complete exercise 3.7

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+const morgan = require('morgan')
+
+console.log(morgan('tiny')); 
 
 let persons = [
   { 
@@ -26,6 +27,18 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -102,6 +115,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 
 const PORT = 3001
